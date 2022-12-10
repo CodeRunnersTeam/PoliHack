90070cee5fcd25301233538dac011c49c9dadf52
Rares03 <67682112+Rares03@users.noreply.github.com>
2022-12-11 00:20:00 +0200

    added list

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:polihack/driver_routes.dart';
 import 'package:polihack/passenger_routes.dart';
+import 'package:polihack/users/driver.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
@@ -24,23 +25,22 @@ class _HomeState extends State<Home> {
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          ButtonTheme(
-              minWidth: 200.0,
-              height: 50.0,
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  setState(() {
-                    currst = !currst;
-                    current = currst ? '     Driver    ' : 'Passenger';
-                    icoana = currst ? Icons.directions_car : Icons.people;
-                  });
-                },
-                icon: Icon(icoana, size: 24.0),
-                label: Text(current),
-              )),
+          ElevatedButton.icon(
+            onPressed: () {
+              setState(() {
+                currst = !currst;
+                current = currst ? '     Driver    ' : 'Passenger';
+                icoana = currst ? Icons.directions_car : Icons.people;
+              });
+            },
+            icon: Icon(icoana, size: 24.0),
+            label: Text(current),
+          ),
           FloatingActionButton(
             onPressed: () {
-              // Add your onPressed code here!
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) =>
+                      currst ? DRoutes() : DriversList(title: "Drivers")));
             },
             backgroundColor: Colors.red,
             child: const Icon(Icons.add),
---
+++ lib/users/driver.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Driver {
+  int id;
+  String name;
+  Driver(this.id, this.name);
+}
+
+class DriversList extends StatefulWidget {
+  const DriversList({super.key, required this.title});
+  final String title;
+  @override
+  _DriversList createState() => _DriversList();
+}
+
+class _DriversList extends State<DriversList> {
+  final List<Driver> drivers = [
+    Driver(1, "Rares"),
+    Driver(2, "Mester"),
+    Driver(3, "Tudor"),
+    Driver(4, "Vlad"),
+    Driver(5, "Dorin"),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: drivers.map((st) {
+          return Card(
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    print("container touch ${st.id}");
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      st.name,
+                      style: TextStyle(fontSize: 50),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
